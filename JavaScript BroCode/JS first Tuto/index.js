a8fd9b1c6d04b769124b08f3e296ff210b715881
Ratan <ratanrathod870@gmail.com>
2023-11-03 10:39:43 +0530

    javaScript BroCode & more about Css Selectors

---
+++ JavaScript BroCode/JS first Tuto/index.js
@@ -0,0 +1,10 @@
+// console.log("Hello");
+// console.log(`I like coding`)
+
+// window.alert("this is an alert")
+// window.alert("I like valorant");
+
+document.getElementById("FirstH").textContent = "Hello";
+document.getElementById("FirstP").textContent = "This is paragraph"
+
+// This is a comment

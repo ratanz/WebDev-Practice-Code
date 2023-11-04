11c578e53d9d276b16e1e76e2b904e761babb25e
Ratan <ratanrathod870@gmail.com>
2023-11-04 13:32:01 +0530

    JS variables bro code

---
+++ JavaScript BroCode/JavaScript VARIABLES/index.js
@@ -0,0 +1,32 @@
+let age = 25;
+let price = 10.11;
+let gpa = 2.5;
+
+let firstName = "Aceu";
+let favoriteFood = "pizza";
+
+// console.log(typeof gpa);
+console.log(`you are ${age} years old`);
+console.log(`the price is ${price}`);
+console.log(`your gpa is: ${gpa}`);
+
+console.log(`Your name is ${firstName}`);
+console.log(`you like ${favoriteFood}`);
+
+// -----------------------------------------
+
+let online = true;
+console.log(`i am online: ${online}`);
+
+let forSale = true;
+console.log(`is this car for sale: ${forSale}`);
+
+// -------------------------------------------------
+
+let fullName = "Aceu";
+let Age = 21;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `You are enrolled: ${isStudent}`;

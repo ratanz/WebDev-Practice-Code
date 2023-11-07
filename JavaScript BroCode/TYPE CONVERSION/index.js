bf1380c8eec5092f8d3f3d9bc8f9f679a731a605
Ratan <ratanrathod870@gmail.com>
2023-11-07 19:24:19 +0530

    CSS GRID ULTIMATE and TypeConversion JS

---
+++ JavaScript BroCode/TYPE CONVERSION/index.js
@@ -0,0 +1,24 @@
+let age = window.prompt("How old are you");
+
+age = Number(age);
+age+=1;
+
+console.log(age, typeof age);
+
+// - - - -   - - - -   - - - -
+
+// let x = "pizza";
+// let y = "burger";
+// let z = "soup";
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof );
+console.log(z, typeof z);

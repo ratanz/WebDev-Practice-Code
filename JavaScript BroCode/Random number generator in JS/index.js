50d2cfd20cf5fb952339dfd6553c99f94dd250a1
Ratan <ratanrathod870@gmail.com>
2023-11-12 14:00:05 +0530

    CSS Transition Property

---
+++ JavaScript BroCode/Random number generator in JS/index.js
@@ -0,0 +1,23 @@
+ 
+//  const min = 50;
+//  const max = 100;
+ 
+//  let randomNum = Math.floor (Math.random() * (max - min)) + min;
+
+
+//  console.log(randomNum);
+
+// ------------------------------------------------------
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+
+const min = 1;
+const max = 7;
+
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}

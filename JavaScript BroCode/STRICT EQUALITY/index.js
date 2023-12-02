564d31ad1c7825c4bfcabaf2f2961b62915b1c91
Ratan <ratanrathod870@gmail.com>
2023-12-02 12:14:24 +0530

    strict equality

---
+++ JavaScript BroCode/STRICT EQUALITY/index.js
@@ -0,0 +1,23 @@
+// = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare both value and type)
+// != not equal to operator
+// !== strict inequality operator (compare both value and type)
+
+const PI = 3.14;
+
+if(PI == "3.14"){
+    console.log("That is Pi");
+} else {
+    confirm.log("That is NOT Pi");
+}
+
+// - - - - - - - - - - - - - -
+
+const PI = 3.14;
+
+if(PI != "3.14"){
+    console.log("That is Pi");
+} else {
+    confirm.log("That is NOT Pi");
+}

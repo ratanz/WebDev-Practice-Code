96960ce387e524654dfc7914960bffd24649fce8
Ratan <ratanrathod870@gmail.com>
2023-12-10 13:06:53 +0530

    for loops

---
+++ JavaScript BroCode/FOR LOOPS/index.js
@@ -0,0 +1,17 @@
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+console.log("Happy Coding");
+
+// - - - - - - - - - - - - - - - - - - -
+
+for (let j = 1; j <= 20; j++) {
+  if (j == 13) {
+    continue; // to skip element or iteration continue can be used.
+    // break;   // to exit out of loop.
+  } else {
+    console.log(i);
+  }
+  console.log(i);
+}

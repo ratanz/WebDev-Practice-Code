98566af57e2d2da402506e340fa99534f93f022f
Ratan <ratanrathod870@gmail.com>
2023-12-11 11:26:49 +0530

    number guessing game bcode & else if ladder harry

---
+++ SigmaDevCourse/Java Script/if else if, else ladder/script.js
@@ -0,0 +1,43 @@
+console.log("Conditionals: if, else if, else ladder");
+
+let age = 0;
+// let grace = 2;
+
+// age += grace;
+// console.log(age);
+
+if (age == 18) {
+    console.log("you can drive R");
+}
+else if(age == 0){
+    console.log("are you kidding");
+}
+else 
+    {
+    console.log("you cannot drive");
+    }
+
+
+
+// console.log(age ** grace);
+
+// if((age + grace) > 18){
+//     console.log("you can drive");
+// }
+
+// else{
+//     console.log("you can't drive");
+// }
+
+a = 7;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+/* translates to : 
+    if(a > b){
+        let c = a - b;
+    }
+    else {
+        let c = a - b; 
+    }
+*/

ea2b1b021ba439e18a4eabfd02f0f4a16402173e
Ratan <ratanrathod870@gmail.com>
2023-12-15 13:29:35 +0530

    arrays bro-code

---
+++ JavaScript BroCode/Arrays/index.js
@@ -0,0 +1,32 @@
+let fruits = ["apple", "mango", "orange", "banana"]
+
+// for(let i = 1; i< fruits.length; i+=2){
+//     console.log(fruits[i])
+// }
+
+// for(let i = fruits.length - 1; i >= 0; i--){
+//     console.log(fruits[i]);
+// }
+
+for(let fruit of fruits){
+    console.log(fruit)
+}
+
+// fruits.sort().reverse();
+// fruits.push("coconut")
+// fruits.pop();
+
+// fruits.shift();
+// fruits.unshift();
+
+// console.log(fruits)
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple")
+
+// console.log(numOfFruits)
+// console.log(index)
---
+++ JavaScript BroCode/Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+fruits.sort().reverse();

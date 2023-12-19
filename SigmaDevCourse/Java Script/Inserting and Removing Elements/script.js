0cd19b892df9f0d603ad10b2e1c2aecb567e1de8
Ratan <ratanrathod870@gmail.com>
2023-12-19 12:33:43 +0530

    Inserting and Removing Elements

---
+++ SigmaDevCourse/Java Script/Inserting and Removing Elements/script.js
@@ -0,0 +1,27 @@
+document.querySelector(".box").innerHTML
+document.querySelector(".container").innerHTML
+// document.querySelector(".box").innerText
+// document.querySelector("container").innerText
+document.querySelector(".container").outerHTML
+document.querySelector(".container").tagName // only apply for element
+document.querySelector(".container").nodeName // apply for every node and element
+document.querySelector(".container").textContent
+document.querySelector(".container").hidden = false;
+document.querySelector(".box").innerHTML = "hey this is ultra box"
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+// document.querySelector(".box").setAttribute("style", "display: inline")
+document.querySelector(".box").attributes
+document.querySelector(".box").remove
+// document.querySelector(".box").dataset 
+// document.querySelector(".container").append(div)
+document.querySelector(".container").classList
+document.querySelector(".container").className
+document.querySelector(".container").classList.add("ratan")
+document.querySelector(".container").classList.remove("red")
+document.querySelector(".container").classList.toggle("bg-red")
+
+
+
+
+

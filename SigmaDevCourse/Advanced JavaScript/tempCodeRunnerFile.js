93d2dcba8a35e68aa827b4388a75b05d2bcaefb7
Ratan <ratanrathod870@gmail.com>
2023-12-27 11:22:06 +0530

    advance Js sigma

---
+++ SigmaDevCourse/Advanced JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+ // let obj = {
+    //     a : 1,
+    //     b : 2, 
+    //     c : 4
+    // }
+
+    // let {a, b} = obj
+    // console.log(a, b)
+
+    // let arr = [1,4,6];
+    // console.log(arr[0] + arr[1] + arr[2])
+    // console.log(sum(...arr))

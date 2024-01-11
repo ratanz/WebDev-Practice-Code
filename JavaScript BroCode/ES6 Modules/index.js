f8a7dba6ff3250596183b41b9062591ca8dca444
Ratan <ratanrathod870@gmail.com>
2024-01-11 10:57:32 +0530

    es 6 modules

---
+++ JavaScript BroCode/ES6 Modules/index.js
@@ -0,0 +1,15 @@
+// ES6 Module = An external file that contains reusable code
+//              that can be imported into other JavaScript files
+//              Can contain variables, classes, functions ... and more
+//              Introduced as part of ECMAScript 2015 update
+
+import { PI, getCircumference, getArea, getVolume } from './mathUtil.js'
+
+console.log(PI);
+const circumference = getCircumference(10);
+const area = getArea(10);
+const volume = getVolume(10);
+
+console.log(`The ${circumference.toFixed(2)} `);
+console.log(`The ${area.toFixed(2)} cm^2 `);
+console.log(`${volume.toFixed(2)} cm^3`);

bc3cb61188d65addcc47f873d6203adffe246c1c
Ratan <ratanrathod870@gmail.com>
2024-01-12 11:41:37 +0530

    Error handling in js

---
+++ JavaScript BroCode/Error handling in js/index.js
@@ -0,0 +1,43 @@
+// Error = An Object that is created to represent a problem that occurs
+//              Occur often with user input or establishing a connection
+
+// try { } = Encloses code that might potentially cause an error
+// catch { } = Catch and handle any thrown Errors from try { }
+// finally { } = (optional) Always executes. Used mostly for clean up
+//                     ex. close files, close connections, release resources
+
+// try{
+//     console.log(x);
+// }
+
+// catch(error){
+//     console.error(error);
+// }
+
+// finally {
+//     console.log("This always excutes")
+// }
+
+// console.log("You have reached the end!")
+
+try {
+    const dividend = Number(window.prompt("Enter a dividend: "))
+    const divisor = Number(window.prompt("Enter a divisor: "))
+
+    if (divisor == 0) {
+        throw new Error("You can't divide by zero!")
+    }
+
+    if (isNaN(dividend) || isNaN(divisor)) {
+        throw new Error("Values must be a number!")
+    }
+
+    const result = dividend / divisor;
+    console.log(result);
+
+}
+catch (e) {
+    console.error(e);
+}
+
+console.log("reached to end of the program")

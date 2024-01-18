7657a704de98e91f5972763fb0ddf9b90d8d6a21
Ratan <ratanrathod870@gmail.com>
2024-01-18 10:56:12 +0530

    hide and show html

---
+++ JavaScript BroCode/HIDE and SHOW HTML in js/index.js
@@ -0,0 +1,14 @@
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
+
+myButton.addEventListener("click", event => {
+
+    if (myImg.style.visibility === "hidden") {
+        myImg.style.visibility = "visible"
+        myButton.textContent = "hide";
+    }
+    else {
+        myImg.style.visibility = "hidden";
+        myButton.textContent = "show";
+    }
+});

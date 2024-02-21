ba22e376a41e2ad3a7857cec0ceecb76575e283f
Ratan <ratanrathod870@gmail.com>
2024-02-21 11:29:22 +0530

    card components

---
+++ React BroCode/Card Components/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="cardImg" src="https://staticg.sportskeeda.com/editor/2023/03/30f51-16793111571149-1920.jpg?w=840" width={250} alt="" />
+            <h2 className="cardTitle">ratan codes</h2>
+            <p className="cardText">i do coding and i play cricket</p>
+        </div>
+    );
+}
+
+export default Card

3185844c3a23eea392917e421edc505c46fe5398
Ratan <ratanrathod870@gmail.com>
2024-03-12 23:42:41 +0530

    useRef hook in react

---
+++ React/useRef hook/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyComponent from './MyComponent'
+
+function App() {
+
+
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ React/useRef hook/src/MyComponent.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect, useRef } from "react"
+
+function MyComponent() {
+
+    const inputRef1 = useRef(null)
+    const inputRef2 = useRef(null)
+    const inputRef3 = useRef(null)
+
+    useEffect(() => {
+        console.log("Component rendered!")
+        // console.log(inputRef)
+    })
+
+    function handleClick1() {
+        inputRef1.current.focus()
+        inputRef1.current.style.backgroundColor = "yellow"
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = ""
+    }
+
+    function handleClick2() {
+        inputRef2.current.focus()
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = "yellow"
+        inputRef3.current.style.backgrounndColor = ""
+    }
+
+    function handleClick3() {
+        inputRef3.current.focus()
+        inputRef1.current.style.backgroundColor = ""
+        inputRef2.current.style.backgroundColor = ""
+        inputRef3.current.style.backgroundColor = "yellow"
+    }
+
+    return (
+        <>
+            <div>
+                <button onClick={handleClick1}>
+                    Click Me 1!
+                </button>
+                <input ref={inputRef1} />
+            </div>
+
+            <div>
+                <button onClick={handleClick2}>
+                    Click Me 2!
+                </button>
+                <input ref={inputRef2} />
+            </div>
+
+            <div>
+                <button onClick={handleClick3}>
+                    Click Me 3!
+                </button>
+                <input ref={inputRef3} />
+            </div>
+        </>
+    )
+}
+
+export default MyComponent

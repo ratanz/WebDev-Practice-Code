7ff0dc7fb0215c0797900b1a5a2f9099d47c9576
Ratan <ratanrathod870@gmail.com>
2024-06-06 14:06:29 +0530

    ejs / dynamic routing

---
+++ Backend Development/EJS, Dynamic routing, Initializing project/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+app.get("/", function (req, res) {
+    res.render("index")
+})
+
+app.get("/profile/:username", function (req, res) {
+    req.params.username
+    res.send(`welcome, ${req.params.username}`)
+})
+
+app.get("/author/:username/:age", function (req, res) {
+    res.send(`welcome, ${req.params.username} of age ${req.params.age}`)
+})
+
+
+
+app.listen(3000, function () {
+    console.log("its running")
+})

d468595cf2ffe109ebcfe231a50397bb51941911
Ratan <ratanrathod870@gmail.com>
2024-06-13 15:36:19 +0530

    added user edit code

--- Backend Development/CRUD Operations with EJS/app.js
+++ Backend Development/CRUD Operations with EJS/app.js
@@ -18,6 +18,18 @@ app.get('/read', async (req, res) => {
     res.render("read", { users })
 })
 
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({ _id: req.params.userid })
+    res.render("edit", { user })
+
+})
+
+app.post('/update/:userid', async (req, res) => {
+    let { image, name, email } = req.body
+    let user = await userModel.findOneAndUpdate({ _id: req.params.userid }, { image, name, email }, { new: true })
+    res.redirect("/read")
+})
+
 app.get('/delete/:id', async (req, res) => {
     let users = await userModel.findOneAndDelete({ _id: req.params.id })
     res.redirect("/read")

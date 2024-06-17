b9c2e7515e275dc57f139f0579d3d59d6d4112cb
Ratan <ratanrathod870@gmail.com>
2024-06-17 15:28:00 +0530

    added post feature / like and edit code for posts

--- Backend Development/Data Association/app.js
+++ Backend Development/Data Association/app.js
@@ -24,9 +24,49 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-    console.log(req.user)
-    res.render('login');
+app.get('/profile', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email }).populate("posts")
+    console.log(user)
+    res.render('profile', { user });
+});
+
+app.get('/like/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user")
+
+    if (post.likes.indexOf(req.user.userid) === -1) {
+        post.likes.push(req.user.userid)
+    }
+    else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1)
+
+    }
+
+    await post.save()
+    res.redirect('/profile');
+});
+
+app.get('/edit/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user")
+
+    res.render("edit", { post })
+});
+
+app.post('/update/:id', isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id}, { content: req.body.content })
+    res.redirect("/profile")
+});
+
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({ email: req.user.email })
+    let { content } = req.body
+
+    let post = await postModel.create({
+        user: user._id,
+        content: content
+    })
+    user.posts.push(post._id)
+    await user.save()
+    res.redirect("/profile")
 });
 
 app.post('/register', async (req, res) => {
@@ -37,10 +77,10 @@ app.post('/register', async (req, res) => {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, async (err, hash) => {
             let user = await userModel.create({
-                email,
-                password: hash,
                 username,
                 name,
+                email,
+                password: hash,
                 age
             })
 
@@ -63,7 +103,7 @@ app.post('/login', async (req, res) => {
         if (result) {
             let token = jwt.sign({ email: email, userid: user._id }, "shhhh")
             res.cookie("token", token)
-            res.status(200).send("You can login.")
+            res.status(200).redirect("/profile")
         }
         else {
             res.redirect("/login")
@@ -77,11 +117,17 @@ app.get('/logout', (req, res) => {
 });
 
 function isLoggedIn(req, res, next) {
-    if (req.cookies.token === "") res.send("You must be logged in")
-    else {
-        let data = jwt.verify(req.cookies.token, "shhhh")
-        req.user = data
-        next()
+    const token = req.cookies.token;
+    if (!token) {
+        return res.redirect("/login");
+    }
+
+    try {
+        const data = jwt.verify(token, "shhhh");
+        req.user = data;
+        next();
+    } catch (err) {
+        res.redirect("/login");
     }
 }
 
--- Backend Development/Data Association/models/user.js
+++ Backend Development/Data Association/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/miniproject")
 
 const userSchema = mongoose.Schema({
-    username: [String], // Change this line
+    username: [String],
     name: String,
     age: Number,
     email: String,
